ff160fa0a1559e66293b8148249bb0be00c4965b
Syafiq Hamidun <syafiqhamidun10@gmail.com>
2023-12-06 14:59:04 +0800

    fix css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// eslint-disable-next-line
+import axios from "axios";
 
 function App() {
+
+  //const url = `https://api.openweathermap.org/data/2.5/weather?q=Klang&appid=50ed9bb0f99e678bbff93f3cef87d35c&units=metric`
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ <div className="app">
+      <div className="container">
+
+        <div className="top">
+          <div className="location">
+            <h2>Klang</h2>
+          </div>
+          <div className="temperature">
+            <h1>32°C</h1>
+          </div>
+          <div className="description">
+            Clouds
+          </div>
+        </div>
+  
+        <div className="bottom">
+          <div className="feels">
+            <p>32°C</p>
+          </div>
+          <div className="humidity">
+            <p>20%</p>
+          </div>
+          <div className="wind">
+            12MPH
+          </div>
+        </div>
+
+
+      </div>
+  </div>
   );
 }
 
